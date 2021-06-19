ff49811971830ad7bc251b3a625d0093c4e1924e
DEEKSHA <DEEKSHA@DESKTOP-KCLGC2B>
2021-06-19 22:33:08 +0530

    first commit

---
+++ bread&milk/src/com/jspiders/pkg1/BreadMilk.java
@@ -0,0 +1,44 @@
+package com.jspiders.pkg1;
+
+import java.util.*;
+class BreadMilk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    String s[]=new String[n];
+		    int a[]=new int[n];
+		    int i=0,h=1;
+		    for(i=0;i<n;i++)
+		    s[i]=sc.next();
+		    for(i=0;i<n;i++)
+		   {
+		       if(s[i].equals("cookie"))
+		       a[i]=1;
+		       else
+		       a[i]=0;
+		   }
+		  
+		   for(i=0;i<n-1;i++)
+		   {
+		       if(a[i]==1&&a[i+1]==1)
+		       {
+		           System.out.println("NO");
+		           h=0;
+		           break;
+		       }
+		   }
+		   if(a[n-1]==1&&h==1)
+		   
+		  System.out.println("NO");
+		   
+		   else if(a[n-1]==0&&h==1)
+		   System.out.println("YES");
+		}
+	}
+}
+
